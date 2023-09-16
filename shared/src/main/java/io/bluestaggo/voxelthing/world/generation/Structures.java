3a94e96c9dbbe63cecb76cad87c132f1c79d662b
NeutralFace177 <jacobaperlow@gmail.com>
2023-09-16 15:19:07 -0700

    a

--- shared/src/main/java/io/bluestaggo/voxelthing/world/generation/Structures.java
+++ shared/src/main/java/io/bluestaggo/voxelthing/world/generation/Structures.java
@@ -3,9 +3,10 @@ package io.bluestaggo.voxelthing.world.generation;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class Structures {
-   ArrayList<ArrayList<ArrayList>> tree1 = new ArrayList<ArrayList<ArrayList>>();
-    
+public enum Structures {
+    Tree1,
+    Tree2
+
 }
 
 
